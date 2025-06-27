050d6d29080cb87ec81d25b3796c814df5f9c3e6
ActuallyaCat42 <157068520+ActuallyaCat42@users.noreply.github.com>
2025-06-28 00:41:08 +0200

    Add files via upload

---
+++ v6/iso_encoder/keymaps/wave/keymap.c
@@ -0,0 +1,159 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "quantum/rgb_matrix/rgb_matrix.h"
+#include "print.h"
+#include "timer.h"
+
+    // clang-format off
+
+
+    // "House" for my X,Y and Time
+    typedef struct {
+        uint8_t x;
+        uint8_t y;
+        uint32_t tick;
+    } hwelle_hit_tracker_t;
+
+    hwelle_hit_tracker_t hwelle_tracker;
+
+    void keyboard_post_init_user(void) {
+        hwelle_tracker.tick = timer_read32() - 2000;
+        hwelle_tracker.x = 255;
+        hwelle_tracker.y = 255;
+    }
+
+enum custom_keycodes {
+    H_WAVE = SAFE_RANGE, // Starts Effeckt
+    C_RGB_HUI, // Hue Increase
+    C_RGB_HUD, //Hue Decrease
+    C_RGB_VAI, //Value Increase
+    C_RGB_VAD // Value Decrease
+};
+
+
+enum layers{
+  MAC_BASE,
+  MAC_FN,
+  WIN_BASE,
+  WIN_FN
+};
+
+#define KC_TASK LGUI(KC_TAB)
+#define KC_FLXP LGUI(KC_E)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC_BASE] = LAYOUT_iso_110(
+        KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  H_WAVE,    KC_NO,    RM_NEXT,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,
+        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+    [MAC_FN] = LAYOUT_iso_110(
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RM_TOGG,  _______,  _______,  RM_TOGG,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,                                _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,            _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,            _______,  _______),
+    [WIN_BASE] = LAYOUT_iso_110(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  RM_PREV,  H_WAVE,   RM_NEXT,  _______,  _______,  _______,  _______,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+    [WIN_FN] = LAYOUT_iso_110(
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RM_TOGG,  _______,  _______,  RM_TOGG,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,                                _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,            _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,            _______,  _______),
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MAC_FN]   = {ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [WIN_BASE] = {ENCODER_CCW_CW(C_RGB_HUD, C_RGB_HUI) },
+    [WIN_FN]   = {ENCODER_CCW_CW(RM_HUED, RM_HUEU) }
+};
+#endif
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // looks for imput and fills valid data into the "house"
+    if (record->event.pressed) {
+        uint8_t row = record->event.key.row;
+        uint8_t col = record->event.key.col;
+        uint8_t led_index = g_led_config.matrix_co[row][col];
+
+        if (led_index != NO_LED) {
+            hwelle_tracker.x = g_led_config.point[led_index].x;
+            hwelle_tracker.y = g_led_config.point[led_index].y;
+            hwelle_tracker.tick = timer_read32();
+
+
+            //
+            uprintf("[WRITER] Set Tracker: x=%u, y=%u\n", hwelle_tracker.x, hwelle_tracker.y);
+            //
+        }
+
+             // what my keycodes will do
+        switch (keycode) {
+            case H_WAVE:
+                rgb_matrix_mode(RGB_MATRIX_CUSTOM_hwelle);
+                return false;
+
+            case C_RGB_HUI:
+                if (rgb_matrix_get_mode() == RGB_MATRIX_CUSTOM_hwelle) {
+                    rgb_matrix_increase_hue_noeeprom();
+                } else {
+                    rgb_matrix_increase_hue();
+                }
+                return false;
+
+            case C_RGB_HUD:
+                if (rgb_matrix_get_mode() == RGB_MATRIX_CUSTOM_hwelle) {
+                    rgb_matrix_decrease_hue_noeeprom();
+                } else {
+                    rgb_matrix_decrease_hue();
+                }
+                return false;
+
+            case C_RGB_VAI:
+                if (rgb_matrix_get_mode() == RGB_MATRIX_CUSTOM_hwelle) {
+                    rgb_matrix_increase_val_noeeprom();
+                } else {
+                    rgb_matrix_increase_val();
+                }
+                return false;
+
+            case C_RGB_VAD:
+                if (rgb_matrix_get_mode() == RGB_MATRIX_CUSTOM_hwelle) {
+                    rgb_matrix_decrease_val_noeeprom();
+                } else {
+                    rgb_matrix_decrease_val();
+                }
+                return false;
+
+        }
+    }
+    return true;
+}
